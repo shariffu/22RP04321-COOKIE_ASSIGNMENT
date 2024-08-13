ba4b39840a3f838aaa7f0dcd547da8ef50f7a6de
shariffu <saidisharifu05@gmail.com>
2024-08-13 10:06:19 +0200

    Add files via upload

---
+++ Session/src/main/java/com/example/servlet/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        // Simple validation for demo purposes
+        if ("GROUP 4".equals(username) && "kabossi".equals(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+            resp.sendRedirect("welcome");
+        } else {
+            resp.sendRedirect("error.html");
+        }
+    }
+}
